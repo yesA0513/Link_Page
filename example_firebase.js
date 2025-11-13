b31d1246c64bcf887eb0e9de69300e765edfc25f
Noa Kim <kimnoa0513@gmail.com>
2025-11-13 22:27:03 +0900

    Add example JS

---
+++ example_firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "Your-API-Key-Here",
+    authDomain: "Your-Auth-Domain-Here",
+    projectId: "Your-Project-ID-Here",
+    storageBucket: "Your-Storage-Bucket-Here",
+    messagingSenderId: "Your-Messaging-Sender-ID-Here",
+    appId: "Your-App-ID-Here",
+    measurementId: "Your-Measurement-ID-Here"
+};
